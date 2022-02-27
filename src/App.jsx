e7f9c93e295bf812d02bca92bbc3c5dab6dcc770
Usama Tufail <miansamusama@gmail.com>
2022-02-28 01:02:30 +0500

    feat(main): shifted and structure all the code for admin side

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import pages, { Error404, dashboardPages } from 'pages';
+// import Error404 from 'pages/error-404/Error404.page';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App bg-custom-main flex items-center content-center">
+      <Suspense fallback={<>Loading...</>}>
+        <Router>
+          <Routes>
+            {pages.map(({ path, Component }) => (
+              <Route key={path} path={path} element={<Component />} exact />
+            ))}
+            <Route path="/dashboard">
+              {dashboardPages.map(({ path, Component }) => (
+                <Route key={path} path={path} element={<Component />} exact />
+              ))}
+            </Route>
+            <Route element={Error404} />
+          </Routes>
+        </Router>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
