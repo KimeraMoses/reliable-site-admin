40ad7657da57eb02b9eb17820186453b05fa8a6e
Usama Tufail <miansamusama@gmail.com>
2022-02-28 16:37:08 +0500

    fix(pages): fixed design issues in common pages

--- src/pages/email-verification/EmailVerification.page.jsx
+++ src/pages/email-verification/EmailVerification.page.jsx
@@ -7,7 +7,7 @@ function EmailVerification() {
     <div className="h-screen w-full flex  items-center justify-content-center">
       <div className="col " style={{ maxWidth: '536px' }}>
         <div className="flex items-center justify-center mb-5">
-          <img src="/con/logo.svg" alt="" className="h-20 w-20" />
+          <img src="/icon/logo.svg" alt="" className="h-20 w-20" />
         </div>
         <div className="bg-custom-secondary  col mx-4 md:mx-auto  rounded-lg p-4 md:p-5">
           <div className="text-center">
@@ -19,7 +19,7 @@ function EmailVerification() {
             </p>
             <button
               type="submit"
-              className="mt-4 custom-blue-bg w-full h-12 rounded-md h-14 text-white font-light"
+              className="bg-blue-500 hover:bg-blue-700 ease-in duration-200 mt-4 w-full h-12 rounded-md text-white font-light"
             >
               {Data.pages.emailVerification.verifyBtn}
             </button>
