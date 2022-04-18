28c6d01ed6021f0fe69c713ae58544751fd740d8
Usama <miansamusama@gmail.com>
2022-04-19 03:18:41 +0500

    feat(Modal): updated modal with fields, onsubmit, validation

--- src/components/Modal/Modal.component.jsx
+++ src/components/Modal/Modal.component.jsx
@@ -1,7 +1,41 @@
-import { Modal as BSModal, Button } from 'react-bootstrap';
+import { Modal as BSModal } from 'react-bootstrap';
+import { Formik, Form, Field } from 'formik';
+import { object, string } from 'yup';
 import './Modal.styles.scss';
 
-export function Modal({ show, setShow, heading, children }) {
+const demoFields = [
+  {
+    title: 'Title',
+    name: 'title',
+    type: 'text',
+    placeholder: 'Please enter title.',
+  },
+  {
+    title: 'Sam',
+    name: 'title',
+    type: 'text',
+    placeholder: 'Please enter title.',
+  },
+];
+
+const vSchema = object({
+  title: string().required('This field is required!'),
+});
+
+const iValues = {
+  title: '',
+};
+
+export function Modal({
+  show,
+  setShow,
+  heading,
+  fields = demoFields,
+  validationSchema = vSchema,
+  initialValues = iValues,
+  submitText = 'Add',
+  handleSubmit = (values) => console.log(values),
+}) {
   const handleClose = () => {
     setShow(false);
   };
@@ -12,7 +46,52 @@ export function Modal({ show, setShow, heading, children }) {
           <h3>{heading}</h3>
         </div>
         <div className="modal__divider" />
-        <div className="modal__body">{children}</div>
+        <div className="modal__body">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+              handleSubmit(values);
+            }}
+          >
+            {() => {
+              return (
+                <Form>
+                  <div className="modal__form">
+                    {fields.map(({ type, name, placeholder, title }) => {
+                      return (
+                        <div className="modal__form-el">
+                          <p className="modal__form-el-label">{title}</p>
+                          <Field
+                            className="modal__form-el-field"
+                            key={name}
+                            type={type}
+                            name={name}
+                            placeholder={placeholder}
+                          />
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="modal__buttons">
+                    <button
+                      onClick={handleClose}
+                      className="modal__buttons-btn modal__buttons-btn-secondary"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="submit"
+                      className="modal__buttons-btn modal__buttons-btn-primary"
+                    >
+                      {submitText}
+                    </button>
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
+        </div>
       </BSModal.Body>
     </BSModal>
   );
