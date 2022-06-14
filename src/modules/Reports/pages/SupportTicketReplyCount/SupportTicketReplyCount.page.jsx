485a9b7cad1fae86ba1b1327e951932a8604413e
Usama <miansamusama@gmail.com>
2022-06-14 13:30:47 +0500

    feat(reports): all reports done

--- src/modules/Reports/pages/SupportTicketReplyCount/SupportTicketReplyCount.page.jsx
+++ src/modules/Reports/pages/SupportTicketReplyCount/SupportTicketReplyCount.page.jsx
@@ -1,57 +1,54 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 import moment from 'moment';
-import { FilterCheck, FilterIndicator } from '../../components';
+// import { FilterCheck, FilterIndicator } from '../../components';
+import { Button, DateRangePicker } from 'components';
+import { Form, Formik } from 'formik';
+// import { useEffect } from 'react';
+import { getReplyReports } from 'store';
+import { useDispatch, useSelector } from 'react-redux';
+import { Spin } from 'antd';
 
-let data = [];
+let dataHolder = [];
 for (let i = 1; i <= 12; i++) {
-  data.push({
-    month: moment(`${i}`, 'M').format('MMM'),
-    byCustomer: i * 20,
-    byAgent: i * 15,
-    byEmail: i * 12,
-    hourlyView: i * 13,
-    linkedIP: i * 6,
-    ticketStatuses: i * 7,
-    ticketDepartments: i * 7,
+  dataHolder.push({
+    createdOn: moment(`${i}`, 'M').format('MMM'),
   });
 }
 
-const getLabel = ({ filterName }) => {
-  switch (filterName) {
-    case 'byCustomer':
-      return 'Tickets by Customer';
-    case 'byAgent':
-      return 'Tickets by Agent';
-    case 'byEmail':
-      return 'Tickets by Email';
-    case 'hourlyView':
-      return 'Hourly View';
-    case 'linkedIP':
-      return 'Linked IP / Server';
-    case 'ticketStatuses':
-      return 'Ticket Statuses';
-    case 'ticketDepartments':
-      return 'Ticket Departments';
-    default:
-      return '';
-  }
-};
+// const getLabel = ({ filterName }) => {
+//   switch (filterName) {
+//     case 'byCustomer':
+//       return 'Tickets by Customer';
+//     case 'byAgent':
+//       return 'Tickets by Agent';
+//     case 'byStatus':
+//       return 'Tickets by Status';
+//     case 'byDepartment':
+//       return 'Tickets by Department';
+//     case 'byPriority':
+//       return 'Tickets by Priority';
+//     default:
+//       return '';
+//   }
+// };
 
 export const SupportTicketReplyCount = () => {
-  const [filters, setFilters] = useState({
-    byCustomer: true,
-    byAgent: true,
-    byEmail: false,
-    hourlyView: false,
-    linkedIP: false,
-    ticketStatuses: false,
-    ticketDepartments: false,
-  });
+  // const [filters, setFilters] = useState({
+  //   byCustomer: true,
+  //   byAgent: true,
+  //   byStatus: false,
+  //   byDepartment: false,
+  //   byPriority: false,
+  // });
+
+  const dispatch = useDispatch();
+  const { replyReports, loading } = useSelector((state) => state?.reports);
+
   return (
     <div className="m-[40px] max-w-[1367px]">
       {/* Filters */}
-      <div className="w-full p-[18px] bg-[#1E1E2D] rounded-[8px]">
+      {/* <div className="w-full p-[18px] bg-[#1E1E2D] rounded-[8px]">
         {Object.keys(filters).map((filter) => {
           return (
             <FilterCheck
@@ -62,15 +59,40 @@ export const SupportTicketReplyCount = () => {
             />
           );
         })}
-      </div>
-
+      </div> */}
       {/* Chart */}
-      <div className="bg-[#1E1E2D] p-[32px] mt-[40px] rounded-[8px]">
-        {/* Filter Indicator */}
-        <div className="h-[52px] flex items-center justify-between">
-          <div></div>
-          <div className="flex items-center gap-[20px]">
-            {Object.keys(filters).map((filter, idx) => {
+      <Spin spinning={loading}>
+        <div className="bg-[#1E1E2D] p-[32px] mt-[40px] rounded-[8px]">
+          {/* Filter Indicator */}
+          <div className="h-[52px] flex items-center justify-between">
+            <div>
+              <Formik
+                onSubmit={async (values) => {
+                  const startDate = moment(values[0]).toISOString();
+                  const endDate = moment(values[1])?.toISOString();
+                  await dispatch(
+                    getReplyReports({
+                      startDate,
+                      endDate,
+                    })
+                  );
+                }}
+                initialValues={{ dates: '' }}
+              >
+                <Form className="flex items-center gap-[20px]">
+                  <div className="min-w-[350px]">
+                    <DateRangePicker name="dates" />
+                  </div>
+                  <div>
+                    <Button type="ghost" htmlType="submit">
+                      Filter Data
+                    </Button>
+                  </div>
+                </Form>
+              </Formik>
+            </div>
+            <div className="flex items-center gap-[20px]">
+              {/* {Object.keys(filters).map((filter, idx) => {
               return (
                 <>
                   {filters?.[filter] ? (
@@ -81,44 +103,46 @@ export const SupportTicketReplyCount = () => {
                   ) : null}
                 </>
               );
-            })}
+            })} */}
+            </div>
           </div>
-        </div>
-        {/* Chart Component */}
-        <div className="mt-[32px]">
-          {/* Heading */}
-          <h5 className="text-[24px] text-white mb-[32px]">
-            Support Response Time
-          </h5>
-          {/* Chart */}
-          <div className="w-full">
-            <ResponsiveContainer width="100%" height={437}>
-              <BarChart
-                barSize={8}
-                barGap={1}
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                  top: 0,
-                  right: 0,
-                  left: 0,
-                  bottom: 0,
-                }}
-              >
-                <XAxis
-                  dataKey="month"
-                  strokeDasharray="3 3"
-                  stroke="#323248"
-                  tick={{ fill: '#474761' }}
-                />
-                <YAxis
-                  width={35}
-                  strokeDasharray="3 3"
-                  stroke="#323248"
-                  tick={{ fill: '#474761' }}
-                />
-                {Object.keys(filters).map((filter, idx) => {
+          {/* Chart Component */}
+          <div className="mt-[32px]">
+            {/* Heading */}
+            <h5 className="text-[24px] text-white mb-[32px]">
+              Support Ticket Reply Count
+            </h5>
+            {/* Chart */}
+            <div className="w-full">
+              {replyReports?.length ? (
+                <ResponsiveContainer width="100%" height={437}>
+                  <BarChart
+                    barSize={30}
+                    // barGap={1}
+                    width={500}
+                    height={300}
+                    data={replyReports?.length ? replyReports : dataHolder}
+                    margin={{
+                      top: 0,
+                      right: 0,
+                      left: 0,
+                      bottom: 0,
+                    }}
+                  >
+                    <XAxis
+                      dataKey="createdOn"
+                      strokeDasharray="3 3"
+                      stroke="#323248"
+                      tick={{ fill: '#474761' }}
+                    />
+                    <YAxis
+                      width={35}
+                      strokeDasharray="3 3"
+                      stroke="#323248"
+                      tick={{ fill: '#474761' }}
+                    />
+                    <Bar dataKey="response" fill={'#FFA800'} />
+                    {/* {Object.keys(filters).map((filter, idx) => {
                   return (
                     <Bar
                       dataKey={filter}
@@ -126,12 +150,18 @@ export const SupportTicketReplyCount = () => {
                       hide={!filters?.[filter]}
                     />
                   );
-                })}
-              </BarChart>
-            </ResponsiveContainer>
+                })} */}
+                  </BarChart>
+                </ResponsiveContainer>
+              ) : (
+                <div className="w-full h-[300px] flex items-center justify-center bg-[#323248] rounded-[8px] text-white text-[16px]">
+                  No Data Available
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      </Spin>
     </div>
   );
 };
--- src/store/Actions/reports.js
+++ src/store/Actions/reports.js
@@ -1,6 +1,16 @@
-import { axios, getAnnualIncome, getError } from 'lib';
+import {
+  axios,
+  getAnnualIncome,
+  getError,
+  getReportsByReplyCount,
+  getReportsByResponseTime,
+} from 'lib';
 import { toast } from 'react-toastify';
-import { getAnnualReportsDispatch, setReportsLoading } from 'store/Slices';
+import {
+  getAnnualReportsDispatch,
+  getResponseReportsDispatch,
+  setReportsLoading,
+} from 'store/Slices';
 
 // Get Annual Reports By Year
 export const getAnnualReports = ({ year }) => {
@@ -10,6 +20,44 @@ export const getAnnualReports = ({ year }) => {
       const { url, config } = getAnnualIncome({ year });
       const res = await axios.get(url, config);
       dispatch(getAnnualReportsDispatch(res?.data?.data?.annualIncomeDetails));
+    } catch (error) {
+      toast.error(getError(error));
+    } finally {
+      dispatch(setReportsLoading(false));
+    }
+  };
+};
+
+// Get Response Reports By Start and End Dates
+export const getResponseReports = ({ startDate, endDate }) => {
+  return async (dispatch) => {
+    dispatch(setReportsLoading(true));
+    try {
+      const { url, config } = getReportsByResponseTime({ startDate, endDate });
+      const res = await axios.get(url, config);
+      dispatch(
+        getResponseReportsDispatch(res?.data?.data?.ticketResponseTimeDetails)
+      );
+    } catch (error) {
+      toast.error(getError(error));
+    } finally {
+      dispatch(setReportsLoading(false));
+    }
+  };
+};
+
+// Get Reply Reports By Start and End Dates
+export const getReplyReports = ({ startDate, endDate }) => {
+  return async (dispatch) => {
+    dispatch(setReportsLoading(true));
+    try {
+      const { url, config } = getReportsByReplyCount({ startDate, endDate });
+      const res = await axios.get(url, config);
+      dispatch(
+        getResponseReportsDispatch(
+          res?.data?.data?.noOfRepliesPerTicketsDetails
+        )
+      );
     } catch (error) {
       toast.error(getError(error));
     } finally {
