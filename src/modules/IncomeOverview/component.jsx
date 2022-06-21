07c40234434389addc998b57b1adc3dc711d8aa3
Usama <miansamusama@gmail.com>
2022-06-21 14:22:19 +0500

    feat(dashboard): added dynamic data to the dashboard

--- src/modules/IncomeOverview/component.jsx
+++ src/modules/IncomeOverview/component.jsx
@@ -41,6 +41,8 @@ const data = [
   },
 ];
 
+const income = false;
+
 export function IncomeOverview() {
   const { t } = useTranslation('/IncomeOverview/ns');
   return (
@@ -49,61 +51,67 @@ export function IncomeOverview() {
         <h3 className="income-overview__header-heading">{t('title')}</h3>
         <p className="income-overview__header-text">{t('desc')}</p>
       </div>
-      <div className="income-overview__chart">
-        <ResponsiveContainer width="100%" height={150}>
-          <LineChart
-            width={500}
-            height={150}
-            data={data}
-            margin={{ top: 0, right: -5, bottom: 0, left: -5 }}
-          >
-            <Line
-              type="monotone"
-              dataKey="income"
-              stroke="#FFA800"
-              strokeWidth={3}
-              dot={false}
-            />
-            <Line
-              type="monotone"
-              dataKey="refunds"
-              stroke="#F64E60"
-              strokeWidth={3}
-              dot={false}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-      <div className="income-overview__card">
-        <div
-          className="income-overview__card-inner"
-          style={{ background: '#392F28 0% 0% no-repeat padding-box' }}
-        >
-          <div className="income-overview__card-inner-icon">
-            <img src="/icon/coin-yellow.svg" alt="" />
+      {!income ? (
+        <div className="text-white p-[20px]">No Data Available!</div>
+      ) : (
+        <>
+          <div className="income-overview__chart">
+            <ResponsiveContainer width="100%" height={150}>
+              <LineChart
+                width={500}
+                height={150}
+                data={data}
+                margin={{ top: 0, right: -5, bottom: 0, left: -5 }}
+              >
+                <Line
+                  type="monotone"
+                  dataKey="income"
+                  stroke="#FFA800"
+                  strokeWidth={3}
+                  dot={false}
+                />
+                <Line
+                  type="monotone"
+                  dataKey="refunds"
+                  stroke="#F64E60"
+                  strokeWidth={3}
+                  dot={false}
+                />
+              </LineChart>
+            </ResponsiveContainer>
           </div>
-          <div
-            className="income-overview__card-inner-text"
-            style={{ color: '#FFA800' }}
-          >
-            {t('yi')}
+          <div className="income-overview__card">
+            <div
+              className="income-overview__card-inner"
+              style={{ background: '#392F28 0% 0% no-repeat padding-box' }}
+            >
+              <div className="income-overview__card-inner-icon">
+                <img src="/icon/coin-yellow.svg" alt="" />
+              </div>
+              <div
+                className="income-overview__card-inner-text"
+                style={{ color: '#FFA800' }}
+              >
+                {t('yi')}
+              </div>
+            </div>
+            <div
+              className="income-overview__card-inner"
+              style={{ background: '#3A2434 0% 0% no-repeat padding-box' }}
+            >
+              <div className="income-overview__card-inner-icon">
+                <img src="/icon/coin-red.svg" alt="" />
+              </div>
+              <div
+                className="income-overview__card-inner-text"
+                style={{ color: '#F64E60' }}
+              >
+                {t('yr')}
+              </div>
+            </div>
           </div>
-        </div>
-        <div
-          className="income-overview__card-inner"
-          style={{ background: '#3A2434 0% 0% no-repeat padding-box' }}
-        >
-          <div className="income-overview__card-inner-icon">
-            <img src="/icon/coin-red.svg" alt="" />
-          </div>
-          <div
-            className="income-overview__card-inner-text"
-            style={{ color: '#F64E60' }}
-          >
-            {t('yr')}
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
--- src/modules/Tickets/component.jsx
+++ src/modules/Tickets/component.jsx
@@ -1,136 +1,181 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PieChart, Pie, Cell } from 'recharts';
+import { useDispatch, useSelector } from 'react-redux';
 import './style.scss';
-
-// Sample data
-const d1 = [
-  { name: 'Un-Assigned', value: 40 },
-  { name: 'Assigned', value: 60 },
-];
-
-// Sample data
-const d2 = [
-  { name: 'Un-Assigned', value: 25 },
-  { name: 'Assigned', value: 75 },
-];
-
-// Sample data
-const d3 = [
-  { name: 'Un-Assigned', value: 0 },
-  { name: 'Assigned', value: 100 },
-];
-
-// Sample data
-const d4 = [
-  { name: 'Un-Assigned', value: 0 },
-  { name: 'Assigned', value: 100 },
-];
+import { getTicketsByAdminID } from 'store';
+import { Spin } from 'antd';
+import { getDepartments } from 'store';
 
 export function Tickets() {
+  const dispatch = useDispatch();
   const { t } = useTranslation('/Tickets/ns');
+
+  const { user } = useSelector((state) => state?.auth);
+  const { tickets, loading } = useSelector((state) => state?.tickets);
+  const { departments } = useSelector((state) => state?.departments);
+  const departmentLoading = useSelector((state) => state?.departments?.loading);
+  useEffect(() => {
+    dispatch(getTicketsByAdminID({ id: user?.id }));
+    dispatch(getDepartments());
+  }, []);
+
+  const ticketsWithDepartmentName = tickets?.map((ticket) => ({
+    ...ticket,
+    departmentName: departments?.filter(
+      (dept) => dept?.id === ticket?.departmentId
+    )[0]?.name,
+  }));
+
+  const finalTickets = ticketsWithDepartmentName?.filter(
+    (ticket) => ticket?.departmentName !== undefined
+  );
+
+  const totalTickets = finalTickets?.length;
+  const counts = {};
+  finalTickets.forEach(function (x) {
+    counts[x?.departmentName] = (counts[x?.departmentName] || 0) + 1;
+  });
+
+  const listToDisplay = Object?.keys(counts)?.map((name) => {
+    return {
+      name,
+      percentage: `${(counts[name] / totalTickets) * 100}%`,
+      percent: (counts[name] / totalTickets) * 100,
+    };
+  });
+
+  const chartData = listToDisplay?.map((el, idx) => {
+    return [
+      {
+        name: 'Un-Assigned',
+        value: 100 - el?.percent,
+      },
+      {
+        name: 'Assigned',
+        value: el?.percent,
+      },
+    ];
+  });
+
+  let minusValue = 0;
+
   return (
     <div className="ticket-card">
-      <div className="ticket-card__header">
-        <h3 className="ticket-card__header-heading">{t('heading')}</h3>
-        <p className="ticket-card__header-text">{t('desc')}</p>
-      </div>
-      <div className="ticket-card__inner">
-        <div className="ticket-card__inner-left">
-          <div className="ticket-card__inner-left-text">
-            <h3
-              className="ticket-card__inner-left-text-heading"
-              style={{ color: '#0BB783' }}
-            >
-              Departament 1
-            </h3>
-            <p className="ticket-card__inner-left-text-txt">60%</p>
+      <Spin spinning={loading || departmentLoading}>
+        <div className="ticket-card__header">
+          <h3 className="ticket-card__header-heading">{t('heading')}</h3>
+          <p className="ticket-card__header-text">{t('desc')}</p>
+        </div>
+        <div className="ticket-card__inner">
+          <div className="ticket-card__inner-left">
+            {listToDisplay?.length ? (
+              listToDisplay?.map(({ name, percentage }, idx) => {
+                return (
+                  <div className="ticket-card__inner-left-text">
+                    <h3
+                      className="ticket-card__inner-left-text-heading"
+                      style={{ color: idx % 2 === 0 ? '#0BB783' : '#3699FF' }}
+                    >
+                      {name}
+                    </h3>
+                    <p className="ticket-card__inner-left-text-txt">
+                      {percentage}
+                    </p>
+                  </div>
+                );
+              })
+            ) : (
+              <h6 className="text-white">No Data To Display...</h6>
+            )}
           </div>
-          <div className="ticket-card__inner-left-text">
-            <h3
-              className="ticket-card__inner-left-text-heading"
-              style={{ color: '#3699FF' }}
-            >
-              Departament 2
-            </h3>
-            <p className="ticket-card__inner-left-text-txt">75%</p>
-          </div>
-          <div className="ticket-card__inner-left-text">
-            <h3
-              className="ticket-card__inner-left-text-heading"
-              style={{ color: '#F64E60' }}
-            >
-              Departament 3
-            </h3>
-            <p className="ticket-card__inner-left-text-txt">100%</p>
-          </div>
-          <div className="ticket-card__inner-left-text">
-            <h3
-              className="ticket-card__inner-left-text-heading"
-              style={{ color: '#8950FC' }}
-            >
-              Departament 4
-            </h3>
-            <p className="ticket-card__inner-left-text-txt">100%</p>
+          <div
+            className="ticket-card__inner-right"
+            style={{ color: '#FFFFFF' }}
+          >
+            {chartData?.length ? (
+              <PieChart width={250} height={250}>
+                {chartData?.map((el, idx) => {
+                  const outerRadius = 120 + minusValue;
+                  const innerRadius = 104 + minusValue;
+                  minusValue = minusValue - 25;
+                  return (
+                    <Pie
+                      key={`chart-ticket-${idx}-assigned-${idx}`}
+                      data={el}
+                      dataKey="value"
+                      stroke="none"
+                      outerRadius={outerRadius}
+                      innerRadius={innerRadius}
+                      endAngle={450}
+                      startAngle={80}
+                      paddingAngle={-10}
+                    >
+                      <Cell fill="#151521" />
+                      <Cell
+                        fill={idx % 2 === 0 ? '#0BB783' : '#3699FF'}
+                        cornerRadius={10}
+                      />
+                    </Pie>
+                  );
+                })}
+                {/* <Pie
+                  data={d1}
+                  dataKey="value"
+                  stroke="none"
+                  outerRadius={120}
+                  innerRadius={104}
+                  endAngle={450}
+                  startAngle={80}
+                  paddingAngle={-10}
+                >
+                  <Cell fill="#151521" />
+                  <Cell fill="#0BB783" cornerRadius={10} />
+                </Pie>
+                <Pie
+                  data={d2}
+                  dataKey="value"
+                  stroke="none"
+                  outerRadius={94}
+                  innerRadius={78}
+                  endAngle={450}
+                  startAngle={80}
+                  paddingAngle={-10}
+                >
+                  <Cell fill="#151521" />
+                  <Cell fill="#3699FF" cornerRadius={10} />
+                </Pie>
+                <Pie
+                  data={d3}
+                  dataKey="value"
+                  stroke="none"
+                  outerRadius={68}
+                  innerRadius={52}
+                  endAngle={450}
+                  startAngle={80}
+                  paddingAngle={-10}
+                >
+                  <Cell fill="#151521" />
+                  <Cell fill="#F64E60" cornerRadius={10} />
+                </Pie>
+                <Pie
+                  data={d4}
+                  dataKey="value"
+                  stroke="none"
+                  outerRadius={42}
+                  innerRadius={26}
+                  endAngle={450}
+                  startAngle={80}
+                  paddingAngle={-10}
+                >
+                  <Cell fill="#151521" />
+                  <Cell fill="#8950FC" cornerRadius={10} />
+                </Pie> */}
+              </PieChart>
+            ) : null}
           </div>
         </div>
-        <div className="ticket-card__inner-right" style={{ color: '#FFFFFF' }}>
-          <PieChart width={250} height={250}>
-            <Pie
-              data={d1}
-              dataKey="value"
-              stroke="none"
-              outerRadius={120}
-              innerRadius={104}
-              endAngle={450}
-              startAngle={80}
-              paddingAngle={-10}
-            >
-              <Cell fill="#151521" />
-              <Cell fill="#0BB783" cornerRadius={10} />
-            </Pie>
-            <Pie
-              data={d2}
-              dataKey="value"
-              stroke="none"
-              outerRadius={94}
-              innerRadius={78}
-              endAngle={450}
-              startAngle={80}
-              paddingAngle={-10}
-            >
-              <Cell fill="#151521" />
-              <Cell fill="#3699FF" cornerRadius={10} />
-            </Pie>
-            <Pie
-              data={d3}
-              dataKey="value"
-              stroke="none"
-              outerRadius={68}
-              innerRadius={52}
-              endAngle={450}
-              startAngle={80}
-              paddingAngle={-10}
-            >
-              <Cell fill="#151521" />
-              <Cell fill="#F64E60" cornerRadius={10} />
-            </Pie>
-            <Pie
-              data={d4}
-              dataKey="value"
-              stroke="none"
-              outerRadius={42}
-              innerRadius={26}
-              endAngle={450}
-              startAngle={80}
-              paddingAngle={-10}
-            >
-              <Cell fill="#151521" />
-              <Cell fill="#8950FC" cornerRadius={10} />
-            </Pie>
-          </PieChart>
-        </div>
-      </div>
+      </Spin>
     </div>
   );
 }
