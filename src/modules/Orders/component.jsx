ab5829d19a2cca86edd41d42a9506ab2fe699685
KimeraMoses <kimeramoses001@gmail.com>
2022-08-05 16:14:41 +0300

    Feat:removed all orders link

--- src/modules/Orders/component.jsx
+++ src/modules/Orders/component.jsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import { LineChart, Line, ResponsiveContainer } from 'recharts';
-import { useTranslation } from 'react-i18next';
-import './style.scss';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { LineChart, Line, ResponsiveContainer } from "recharts";
+import { useTranslation } from "react-i18next";
+import "./style.scss";
+import { useNavigate } from "react-router-dom";
 
 const data = [
   {
-    name: '25-Feb',
+    name: "25-Feb",
     all: 50,
     your: 80,
   },
   {
-    name: '26-Feb',
+    name: "26-Feb",
     all: 30,
     your: 20,
   },
   {
-    name: '28-Feb',
+    name: "28-Feb",
     all: 90,
     your: 50,
   },
   {
-    name: '01-Mar',
+    name: "01-Mar",
     all: 30,
     your: 20,
   },
   {
-    name: '02-Mar',
+    name: "02-Mar",
     all: 85,
     your: 50,
   },
   {
-    name: '02-Mar',
+    name: "02-Mar",
     all: 25,
     your: 30,
   },
   {
-    name: '02-Mar',
+    name: "02-Mar",
     all: 90,
     your: 50,
   },
@@ -44,12 +44,12 @@ const data = [
 
 export function Orders() {
   const navigate = useNavigate();
-  const { t } = useTranslation('/Orders/ns');
+  const { t } = useTranslation("/Orders/ns");
   return (
     <div className="order">
       <div className="order__header">
-        <h3 className="order__header-heading">{t('title')}</h3>
-        <p className="order__header-text">{t('desc')}</p>
+        <h3 className="order__header-heading">{t("title")}</h3>
+        <p className="order__header-text">{t("desc")}</p>
       </div>
       <div className="order__chart">
         <ResponsiveContainer width="100%" height={150}>
@@ -79,29 +79,29 @@ export function Orders() {
       <div className="order__card">
         <div
           className="order__card-inner cursor-pointer"
-          style={{ background: '#212e48 0% 0% no-repeat padding-box' }}
+          style={{ background: "#212e48 0% 0% no-repeat padding-box" }}
           onClick={() => {
-            navigate('/admin/dashboard/billing/orders/all-orders/list');
+            navigate("/admin/dashboard/billing/orders/all-orders/list");
           }}
         >
           <div className="order__card-inner-icon">
             <img src="/icon/bulk-blue.svg" alt="" />
           </div>
-          <div className="order__card-inner-text " style={{ color: '#3699FF' }}>
-            {t('all')}
+          <div className="order__card-inner-text " style={{ color: "#3699FF" }}>
+            {t("all")}
           </div>
         </div>
         <div
           className="order__card-inner cursor-pointer"
-          style={{ background: '#1C3238 0% 0% no-repeat padding-box' }}
+          style={{ background: "#1C3238 0% 0% no-repeat padding-box" }}
           onClick={() => {
-            navigate('/admin/dashboard/billing/orders/your-orders/list');
+            navigate("/admin/dashboard/billing/orders/your-orders/list");
           }}
         >
           <div className="order__card-inner-icon">
             <img src="/icon/bulk-green.svg" alt="" />
           </div>
-          <div className="order__card-inner-text " style={{ color: '#0BB783' }}>
+          <div className="order__card-inner-text " style={{ color: "#0BB783" }}>
             Your Orders
           </div>
         </div>
